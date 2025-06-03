a5cba9c2c4c1f100c0d44513dda706b7f06f2132
notissoedy <71132703+notissoedy@users.noreply.github.com>
2025-06-03 21:03:35 +0200

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
       x.className += " responsive";
     } else {
-      x.className = "navbar";
+      x.className = "topnav";
     }
   }
